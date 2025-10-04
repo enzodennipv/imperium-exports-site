7cf8a1a1114820432fb935ff94dc716f93959e86
Enzo Denni <enzodenni.pv@gmail.com>
2025-10-04 17:36:07 +0200

    Initial commit for deployment

---
+++ app/locale/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutPage() {
+    return (
+      <main className="p-6">
+        <h1>About Imperium Exports</h1>
+        <p>Some description…</p>
+      </main>
+    );
+  }
+  
