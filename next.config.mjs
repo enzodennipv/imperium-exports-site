c7faba4f800ad1efbc1ff69e91c32e91d3cc33c8
Enzo Denni <enzodenni.pv@gmail.com>
2025-11-06 23:59:36 +0100

    Initial commit for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextconfig = {
   images: {
     remotePatterns: [
       { protocol: 'http', hostname: 'static.photos', pathname: '/**' },
@@ -9,3 +9,20 @@ const nextConfig = {
   }
 };
 export default nextConfig;
+rewrites, redirects, basePath, i18n
+
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Désactive provisoirement tout rewrite ou redirect
+  // pour éliminer les causes de 404/500
+  async rewrites() {
+    return [];
+  },
+  async redirects() {
+    return [];
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
